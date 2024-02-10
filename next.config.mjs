2c938343264d7fb1dcb30a0866716f3b0da42c04
pheonixeye <drkazgm@gmail.com>
2024-02-10 22:12:07 +0200

    next-config edge

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    runtime: "edge",
+  },
+  reactStrictMode: true,
+  swcMinify: true,
+};
 
 export default nextConfig;
